0dd5865c142ba263d54deaeac348a3c2b459245a
ilunga lebreton <ilungalebreton@gmail.com>
2020-03-28 21:34:07 -0400

    commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://lebretoni:Idl2014.@barista-app-ix2lx.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'barista'
+};
